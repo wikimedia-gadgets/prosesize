f5debb66a2eb862592fde7a1d9e1cd5ac228b086
Galobtter <galobtter@gmail.com>
2019-10-11 16:21:52 -0700

    Mostly working version

--- prosesize.js
+++ prosesize.js
@@ -1,234 +1,158 @@
-/* eslint-disable */
 // rewrite of [[User:Dr_pda/prosesize.js]]
-// TODO: modernize (User:Shubinator/DYKcheck.js may have useful code? or use xtools api ) and fix to work on all maintained skins propose to become gadget
-// Wikipedia:Village_pump_(technical)/Archive_161#"Page_size"_Tool:_"Word_count"_script_'User:Dr_pda/prosesize.js'_does_not_work_with_skin_=_Timeless
-function loadXMLDocPassingTemplate( url, handler, page ) {
-	// branch for native XMLHttpRequest object
-	if ( window.XMLHttpRequest ) {
-		var req = new XMLHttpRequest();
-	}
-	// branch for IE/Windows ActiveX version
-	else if ( window.ActiveXObject ) {
-		var req = new ActiveXObject( 'Microsoft.XMLHTTP' );
-	}
-	if ( req ) {
-		req.onreadystatechange = function () {
-			handler( req, page );
-		};
-		req.open( 'GET', url, true );
-		req.send( '' );
-	}
-}
-
-function getWikiText( req, page ) {
-	// only if req shows "loaded"
-	if ( req.readyState === 4 ) {
-		// only if "OK"
-		if ( req.status === 200 ) {
-			// ...processing statements go here...
-			response = req.responseXML.documentElement;
-			var rev = response.getElementsByTagName( 'rev' );
-			if ( rev.length > 0 ) {
-				var result = rev[ 0 ].getAttribute( 'size' );
-				if ( result > 10240 ) {
-					result = ( result / 1024 ).toFixed( 0 ) + '&nbsp;kB';
-				} else {
-					result = result + '&nbsp;B';
-				}
-				wiki_value = document.createElement( 'li' );
-				wiki_value.id = 'wiki-size';
-				wiki_value.innerHTML = '<b>Wiki text: </b>' + result;
-				var output = document.getElementById( 'document-size-stats' );
-				prose_value = document.getElementById( 'prose-size' );
-				output.insertBefore( wiki_value, prose_value );
-			} else {
-				// alert("There was a problem using the Wikipedia Search to find the wiki text size\nEither the search is not working or the correct article did not appear on the first page of results");
-				wiki_value = document.createElement( 'li' );
-				wiki_value.id = 'wiki-size';
-				wiki_value.innerHTML = '<b>Wiki text: </b>Problem getting wiki text size';
-				var output = document.getElementById( 'document-size-stats' );
-				prose_value = document.getElementById( 'prose-size' );
-				output.insertBefore( wiki_value, prose_value );
-			}
+( function () {
+	function sizeFormatter( size ) {
+		if ( size > 10240 ) {
+			return ( ( size / 1024 ).toFixed( 0 ) + '&nbsp;kB' );
 		} else {
-			alert( 'There was a problem retrieving the XML data:\n' +
-				req.statusText );
+			return ( size + '&nbsp;B' );
 		}
 	}
-}
 
-function getFileSize( req, page ) {
-	// only if req shows "loaded"
-	if ( req.readyState == 4 ) {
-		// only if "OK"
-		if ( req.status == 200 ) {
-			// ...processing statements go here...
-			var fsize = req.responseText.length;
-			window.status = fsize;
-			var total_value = document.createElement( 'li' );
-			total_value.id = 'total-size';
-			total_value.innerHTML = '<b>File size: </b>' + ( fsize / 1024 ).toFixed( 0 ) + '&nbsp;kB';
-			var output = document.getElementById( 'document-size-stats' );
-			var prose_html_value = document.getElementById( 'prose-size-html' );
-			output.insertBefore( total_value, prose_html_value );
-		} else {
-			alert( 'There was a problem retrieving the XML data:\n' +
-				req.statusText + '\n(' + url + ')' );
-		}
-	}
-}
-
-function getLength( id ) {
-	var textLength = 0;
-	for ( var i = 0; i < id.childNodes.length; i++ ) {
-		if ( id.childNodes[ i ].nodeType === Node.TEXT_NODE ) {
-			textLength += id.childNodes[ i ].nodeValue.length;
-		} else if ( id.childNodes[ i ].nodeType === Node.ELEMENT_NODE &&
-			( id.childNodes[ i ].id == 'coordinates' || id.childNodes[ i ].className.indexOf( 'emplate' ) != -1 ) ) {
-			// special case for {{coord}} and {{fact}}-like templates
-			// Exclude from length, and don't set background yellow
-			id.childNodes[ i ].addClass( 'prosesize-special-template' );
-		} else {
-			textLength += getLength( id.childNodes[ i ] );
-		}
-	}
-	return textLength;
-}
-
-function getRefMarkLength( id, html ) {
-	var textLength = 0;
-	for ( var i = 0; i < id.childNodes.length; i++ ) {
-		if ( id.childNodes[ i ].nodeType === Node.ELEMENT_NODE && id.childNodes[ i ].className === 'reference' ) {
-			textLength += ( html ) ? id.childNodes[ i ].innerHTML.length : getLength( id.childNodes[ i ] );
-		}
-	}
-	return textLength;
-}
-
-function sizeFormatter( size ) {
-	if ( size > 10240 ) {
-		return ( ( size / 1024 ).toFixed( 0 ) + '&nbsp;kB' );
-	} else {
-		return ( size + '&nbsp;B' );
-	}
-}
-
-function getDocumentSize() {
-	var bodyContent = $( '.mw-parser-output' );
-
-	if ( bodyContent.length === 0 ) {
-		return;
+	function sizeElement( id, text, size, extraText ) {
+		return $( '<li>' )
+			.prop( 'id', id )
+			.html( $( '<b>' ).text( text ).append( ' ' + sizeFormatter( size ) + ( extraText || '' ) ) );
 	}
 
-	if ( $( 'document-size-stats' ).length ) {
-		// if statistics already exist, turn them off and remove highlighting
-		$( 'document-size-stats' ).remove();
-		$( 'document-size-header' ).remove();
-		bodyContent.children( 'p' ).removeClass( 'prosesize-highlight' );
-	} else {
+	function getRevisionSize( proseValue ) {
 		var Api = new mw.Api();
-		output = $( 'ul' ).prop( 'id', 'document-size-stats' );
-
-		var prose_html_value = $( 'li' ).prop( 'id', 'prose-size-html' );
-		var ref_html_value = $( 'li' ).prop( 'id', 'ref-size-html' );
-		var prose_value = $( 'li' ).prop( 'id', 'prose-size' );
-		var ref_value = $( 'li' ).prop( 'id', 'ref-size' );
-
-		output.append( prose_html_value, ref_html_value, prose_value, ref_value );
-
-		var header = $( 'span' )
-			.prop( 'id', 'document-size-header' )
-			.html( '<br/>Document statistics: <small><i>(See <a href="//en.wikipedia.org/wiki/Wikipedia:Prosesize">here</a> for details.)<i></small>' );
-
-		bodyContent.prepend( header, output );
-
-		// File size not well defined for preview mode or section edit
-		if ( mw.config.get( 'wgAction' ) != 'submit' ) {
-			loadXMLDocPassingTemplate( location.pathname, getFileSize, '' );
+		function appendResult( size ) {
+			var wikiValue = sizeElement( 'wiki-size', 'Wiki text:', size );
+			proseValue.before( wikiValue );
 		}
-		// Calculate prose size and size of reference markers ([1] etc)
-		var pList = bodyContent.children( 'p' );
-
-		prose_size = 0;
-		prose_size_html = 0;
-		refmark_size = 0;
-		refmark_size_html = 0;
-		word_count = 0;
-		for ( var i = 0; i < pList.length; i++ ) {
-			var para = pList[ i ];
-			if ( para.parentNode == bodyContent ) {
-				prose_size += getLength( para );
-				prose_size_html += para.innerHTML.length;
-				refmark_size += getRefMarkLength( para, false );
-				refmark_size_html += getRefMarkLength( para, true );
-				word_count += para.innerHTML.replace( /(<([^>]+)>)/ig, '' ).split( ' ' ).length;
-				para.classes += ' prosesize-highlight';
-			}
-		}
-
-		prose_value.innerHTML = '<b>Prose size (text only): </b>' + sizeFormatter( prose_size - refmark_size ) + ' (' + word_count + ' words) "readable prose size"';
-		prose_html_value.innerHTML = '<b>Prose size (including all HTML code): </b>' + sizeFormatter( prose_size_html - refmark_size_html );
-
-		// Calculate size of references (i.e. output of <references/>)
-		var rList = bodyContent.getElementsByTagName( 'ol' );
-		var ref_size = 0;
-		var ref_size_html = 0;
-		for ( var i = 0; i < rList.length; i++ ) {
-			if ( rList[ i ].parentNode.className == 'references' ) {
-				ref_size = getLength( rList[ i ] );
-				ref_size_html = rList[ i ].innerHTML.length;
-			}
-		}
-
-		ref_value.innerHTML = '<b>References (text only): </b>' + sizeFormatter( ref_size + refmark_size );
-		ref_html_value.innerHTML = '<b>References (including all HTML code): </b>' + sizeFormatter( ref_size_html + refmark_size_html );
-
-		if ( mw.config.get( 'wgAction' ) == 'submit' ) {
-			// Get size of text in edit box
-			result = $( '#wpTextbox1' ).length;
-			if ( result > 10240 ) {
-				result = ( result / 1024 ).toFixed( 0 ) + '&nbsp;kB';
-			} else {
-				result = result + '&nbsp;B';
-			}
-			wiki_value = $( 'li' )
-				.prop('id', 'wiki-size')
-				.html( '<b>Wiki text: </b>' + result );
-			var output = $( 'document-size-stats' );
-			$( 'prose-size' ).before(wiki_value);
-		} else {
+		if ( mw.config.get( 'wgAction' ) === 'submit' ) {
+		// Get size of text in edit box
+		// eslint-disable-next-line no-jquery/no-global-selector
+			appendResult( $( '#wpTextbox1' ).html().length );
+		} else if ( mw.config.get( 'wgIsArticle' ) ) {
 			// Get revision size from API
-			Apiresult = Api.get( {
+			Api.get( {
+				action: 'query',
 				prop: 'revisions',
 				rvprop: 'size',
 				revids: mw.config.get( 'wgRevisionId' ),
 				formatversion: 2
-			} );
-			loadXMLDocPassingTemplate( searchURL, getWikiText, mw.config.get( 'wgPageName' ).replace( /_/g, ' ' ) );
+			} ).then( function ( result ) {
+				appendResult( result.query.pages[ 0 ].revisions[ 0 ].size );
+			}
+			);
 		}
 	}
-}
 
-$.when( $.ready, mw.loader.using( [ 'mediawiki.api', 'mediawiki.util' ] ) ).then( function () {
-	// Depending on whether in edit mode or preview/view mode, show the approppiate response upon clicking the portlet link
-	var func, $portlet;
-	if ( mw.config.get( 'wgAction' ) == 'edit' || ( mw.config.get( 'wgAction' ) == 'submit' && document.getElementById( 'wikiDiff' ) ) ) {
-		func = function () {
-			alert( 'You need to preview the text for the prose size script to work in edit mode.' );
-		};
-		$portlet.addClass( 'prosesize-portlet-link-edit-mode' );
-	} else if ( [ 'view', 'submit', 'historysubmit', 'purge' ].indexOf( mw.config.get( 'wgAction' ) ) !== -1 ) {
-		func = function () {
-			getDocumentSize();
-		};
+	function getFileSize( proseHtmlValue ) {
+	// File size not well defined for preview mode or section edit
+		if ( mw.config.get( 'wgAction' ) !== 'submit' ) {
+			$.get( location ).then( function ( result ) {
+				var fsize = sizeElement( 'total-size', 'File size:', result.length );
+				proseHtmlValue.prepend( fsize );
+			} );
+		}
 	}
-	if ( func ) {
-		$portlet = $( mw.util.addPortletLink( 'p-tb', '#', 'Page size', 't-page-size', 'Calculate page and prose size' ) );
-		$portlet.on( 'click', function ( e ) {
-			e.preventDefault();
-			func();
+
+	function getLength( id ) {
+		var i;
+		var textLength = 0;
+		for ( i = 0; i < id.childNodes.length; i++ ) {
+			if ( id.childNodes[ i ].nodeType === Node.TEXT_NODE ) {
+				textLength += id.childNodes[ i ].nodeValue.length;
+			} else if ( id.childNodes[ i ].nodeType === Node.ELEMENT_NODE &&
+			( id.childNodes[ i ].id === 'coordinates' || id.childNodes[ i ].className.indexOf( 'emplate' ) !== -1 ) ) {
+			// special case for {{coord}} and {{fact}}-like templates
+			// Exclude from length, and don't set background yellow
+				id.childNodes[ i ].addClass( 'prosesize-special-template' );
+			} else {
+				textLength += getLength( id.childNodes[ i ] );
+			}
+		}
+		return textLength;
+	}
+
+	function getRefMarkLength( id, html ) {
+		var i;
+		var textLength = 0;
+		for ( i = 0; i < id.childNodes.length; i++ ) {
+			if ( id.childNodes[ i ].nodeType === Node.ELEMENT_NODE && id.childNodes[ i ].className === 'reference' ) {
+				textLength += ( html ) ? id.childNodes[ i ].innerHTML.length : getLength( id.childNodes[ i ] );
+			}
+		}
+		return textLength;
+	}
+
+	function main() {
+		var proseValue, refValue, refHtmlValue, proseHtmlValue;
+		// eslint-disable-next-line no-jquery/no-global-selector
+		var bodyContent = $( '.mw-parser-output' );
+		// eslint-disable-next-line no-jquery/no-global-selector
+		var prevStats = $( 'document-size-stats' );
+		var proseSize = 0;
+		var proseSizeHtml = 0;
+		var refmarksize = 0;
+		var refmarkSizeHtml = 0;
+		var wordCount = 0;
+		var refSize = 0;
+		var refSizeHtml = 0;
+		var header = $( '<span>' )
+			.prop( 'id', 'document-size-header' )
+			.html( '<br/>Document statistics: <small><i>(See <a href="//en.wikipedia.org/wiki/Wikipedia:Prosesize">here</a> for details.)<i></small>' );
+		var output = $( '<ul>' ).prop( 'id', 'document-size-stats' );
+		if ( bodyContent.length === 0 ) {
+			return;
+		}
+		if ( prevStats.length ) {
+		// if statistics already exist, turn them off and remove highlighting
+			prevStats.remove();
+			prevStats.children( 'document-size-header' ).remove();
+			bodyContent.children( 'p' ).removeClass( 'prosesize-highlight' );
+		} else {
+			// Calculate prose size and size of reference markers ([1] etc)
+
+			bodyContent.children( 'p' ).each( function () {
+				proseSize += getLength( this );
+				proseSizeHtml += this.innerHTML.length;
+				refmarksize += getRefMarkLength( this, false );
+				refmarkSizeHtml += getRefMarkLength( this, true );
+				wordCount += this.innerHTML.replace( /(<([^>]+)>)/ig, '' ).split( ' ' ).length;
+				$( this ).addClass( 'prosesize-highlight' );
+			} );
+
+			// Calculate size of references (i.e. output of <references/>)
+			bodyContent.find( 'ol.references' ).each( function () {
+				refSize = getLength( this );
+				refSizeHtml = this.innerHTML.length;
+			} );
+
+			proseValue = sizeElement( 'prose-size', 'Prose size (text only):', proseSize - refmarksize, ' (' + wordCount + ' words) "readable prose size"' );
+			refValue = sizeElement( 'ref-size', 'References (text only):', refSize + refmarksize );
+			refHtmlValue = sizeElement( 'ref-size-html', 'References (including all HTML code):', refSizeHtml + refmarkSizeHtml, ' (' + wordCount + ' words) "readable prose size"' );
+			proseHtmlValue = sizeElement( 'prose-size-html', 'Prose size (including all HTML code):', proseSizeHtml - refmarkSizeHtml );
+			output.append( proseHtmlValue, refHtmlValue, proseValue, refValue );
+			bodyContent.prepend( header, output );
+			getFileSize( proseHtmlValue );
+			getRevisionSize( proseValue );
+		}
+	}
+
+	if (
+		!mw.config.get( 'wgCanonicalSpecialPageName' )
+	) {
+		$.when( $.ready, mw.loader.using( [ 'mediawiki.api', 'mediawiki.util' ] ) ).then( function () {
+			// Depending on whether in edit mode or preview/view mode, show the approppiate response upon clicking the portlet link
+			var func, $portlet;
+			if ( mw.config.get( 'wgAction' ) === 'edit' || ( mw.config.get( 'wgAction' ) === 'submit' && document.getElementById( 'wikiDiff' ) ) ) {
+				func = function () {
+					mw.notify( 'You need to preview the text for the prose size script to work in edit mode.' );
+				};
+				$portlet.addClass( 'prosesize-portlet-link-edit-mode' );
+			} else if ( [ 'view', 'submit', 'historysubmit', 'purge' ].indexOf( mw.config.get( 'wgAction' ) ) !== -1 ) {
+				func = main;
+			}
+			if ( func ) {
+				$portlet = $( mw.util.addPortletLink( 'p-tb', '#', 'Page size', 't-page-size', 'Calculate page and prose size' ) );
+				$portlet.on( 'click', function ( e ) {
+					e.preventDefault();
+					func();
+				} );
+			}
 		} );
 	}
-} );
-
-// </nowiki>
+}() );
